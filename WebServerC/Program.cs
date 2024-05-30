3bc602911fbb8a8c1875f7c5d97d99969aa46b0e
Cerjo-axe <58487647+Cerjo-axe@users.noreply.github.com>
2024-05-29 23:08:36 -0300

    skeleton web Server

---
+++ WebServerC/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebServer
+{
+    public class Program
+    {
+         
+
+        public static void Main(string[] args)
+        {
+            Start();
+        }
+
+        public static void Start()
+        {
+            IPHostEntry ipHostInfo =Dns.GetHostEntry("localhost");
+            IPAddress iPAddress = ipHostInfo.AddressList[1];
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress,42069);
+            try
+            {
+                using Socket listener = new Socket(localEndPoint.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                Console.WriteLine("Awaiting for a message");
+                Socket clientConnection = listener.Accept();
+                while (true)
+                {
+                    byte[] buffer = new byte[2048];
+                    int receivedData = clientConnection.Receive(buffer,SocketFlags.None);
+                    string data = Encoding.UTF8.GetString(buffer, 0 , receivedData);
+                    Console.WriteLine(data);
+                    byte[] returnMsg = Encoding.UTF8.GetBytes("" +
+                "HTTP/1.1 404 Not Found\r\n" +
+                "Content-Type: text/html\r\n" +
+                "\r\n" +"Awaiting for a message");
+                    clientConnection.Send(returnMsg);
+                }
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+        }
+
+    }
+}
